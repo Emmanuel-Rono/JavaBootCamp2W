c7ba3bd1a3372181ccca72af43dfc586188e261e
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2024-08-13 16:36:26 +0300

    Arrays

---
+++ ArraysRefr.java
@@ -0,0 +1,15 @@
+// imort utill package
+import java.util.ArrayList;
+
+class ArraysRefr
+{
+       public static void main(String[] args)
+    {
+        //crete object of the implementing class
+
+        ArrayList<String> animals = new ArrayList<>();
+        animals.add("Hyn");
+        animals.add("ccw");
+        System.out.println("ArrayList: " + animals);
+    }
+}
